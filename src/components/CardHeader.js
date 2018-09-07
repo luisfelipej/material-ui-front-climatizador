3f5b3e680b52a3189987e80ee06fd25484d2e1db
luisfelipej97 <luisfelipej97@gmail.com>
2018-09-07 14:05:15 -0300

    Se arreglo CardIcon adaptable todo tamaño

--- src/components/CardHeader.js
+++ src/components/CardHeader.js
@@ -4,7 +4,7 @@ import { Typography } from '@material-ui/core';
 const CardHeader = ({...props}) => (
     <div className="cardHeader">
         <div className="cardHeader-title">
-            <Typography>{ props.title }</Typography>
+            <Typography variant={props.variant} color="inherit">{ props.title }</Typography>
         </div>
         {props.children}
     </div>
